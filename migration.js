608a2d71c923674e9a975edee130f6cfd162e51a
Mukul <kmukul1197@gmail.com>
2021-04-02 16:11:21 +0530

    Script for creating relational schema on production db

---
+++ migration.js
@@ -0,0 +1,31 @@
+const { Client } = require('pg')
+var isTrueSet = (process.env.Ssl === 'true');
+const client = new Client({
+	user: process.env.user,
+	host: process.env.hostName,
+	database: process.env.dataBase,
+	password: process.env.passWord,
+	port: process.env.portNumber,
+	ssl: {
+		rejectUnauthorized: false
+	}
+})
+client.connect(function (err) {
+    if (err) {
+        console.log("Error in connection with database");
+		console.log("error", err);
+    }
+    else {
+        console.log("Conection Established");
+		var qry  = 'create table third_blog_table(id int, title varchar(60), content text)';
+					client.query(qry, (err, res) => {
+						if (err) {
+							console.log('err for', err, qry)
+						}
+						else {
+							console.log('Created');
+						}
+					})
+    }
+})
+console.log('done');
